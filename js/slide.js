82c6a5f26fe22a193cc5a7f1f914cf7d23b8a48f
Felipe Acelino <felipeacelino@hotmail.com>
2019-05-22 01:53:26 -0300

    Cálculo de distancias

--- js/slide.js
+++ js/slide.js
@@ -3,22 +3,38 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
+    this.dist = { finalPosition: 0, startX: 0, movement: 0 };
+  }
+
+  // Move o slide
+  moveSlide(distX) {
+    this.dist.movePosition = distX;
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+  }
+
+  // Atualiza a posição do movimento
+  updatePosition(clientX) {
+    this.dist.movement = (this.dist.startX - clientX) * 1.6;
+    return this.dist.finalPosition - this.dist.movement;
   }
 
   // Inicia o evento
   onStart(event) {
     event.preventDefault();
+    this.dist.startX = event.clientX;
     this.wrapper.addEventListener('mousemove', this.onMove);
   }
 
   // Evento de movimento
-  onMove() {
-    
+  onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX);
+    this.moveSlide(finalPosition);
   }
 
   // Finaliza o evento
   onEnd() {
     this.wrapper.removeEventListener('mousemove', this.onMove);
+    this.dist.finalPosition = this.dist.movePosition;
   }
 
   // Adiciona os eventos ao slide
